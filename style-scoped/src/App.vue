ea9d4a313c158a1395033ee47c9f7c2f8c0abcde
JJK <chang1381023@gmail.com>
2024-07-21 16:26:49 +0800

    Add Nodes

---
+++ style-scoped/src/App.vue
@@ -0,0 +1,26 @@
+<!-- App.vue -->
+<template>
+  <div>
+    <h1>Style-scoped Attribute 範例</h1>
+    <ComponentA/>
+    <ComponentB/>
+  </div>
+</template>
+
+<script>
+import ComponentA from "./components/ComponentA.vue";
+import ComponentB from "./components/ComponentB.vue";
+
+export default {
+  name: "App",
+  components: {
+    ComponentA,
+    ComponentB
+  },
+  methods: {
+    appEvent() {
+      console.log("appEvent occured");
+    },
+  },
+};
+</script>
---
+++ style-scoped/src/components/ComponentA.vue
@@ -0,0 +1,11 @@
+<!-- A.vue -->
+<template>
+  <p>Component A</p>
+</template>
+
+<style>
+/* <style scoped> */
+  p {
+    color: red; /* 將 colr 改為 color */
+  }
+</style>
